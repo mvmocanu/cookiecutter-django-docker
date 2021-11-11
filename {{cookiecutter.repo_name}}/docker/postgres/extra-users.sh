5affe02259b5f869085ffaf05d56e775e8efdb76
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-11-11 08:53:05 +0200

    Handle situations with broken permissions.

--- {{cookiecutter.repo_name}}/docker/postgres/extra-users.sh
+++ {{cookiecutter.repo_name}}/docker/postgres/extra-users.sh
@@ -7,4 +7,28 @@ for extra in ${POSTGRES_EXTRAS:-}; do
         CREATE DATABASE $extra;
         GRANT ALL PRIVILEGES ON DATABASE $extra TO $extra;
 EOSQL
+    # Force a reassign in case something was misconfigured in the past
+    psql $extra <<-EOSQL
+        DO \$\$
+        DECLARE r record;
+        BEGIN
+            FOR r IN
+                select 'ALTER TABLE "' || table_schema || '"."' || table_name || '" OWNER TO $extra;' as a
+                    from information_schema.tables where table_schema = 'public'
+                    union all
+                select 'ALTER TABLE "' || sequence_schema || '"."' || sequence_name || '" OWNER TO $extra;' as a
+                    from information_schema.sequences where sequence_schema = 'public'
+                    union all
+                select 'ALTER TABLE "' || table_schema || '"."' || table_name || '" OWNER TO $extra;' as a
+                    from information_schema.views where table_schema = 'public'
+                    union all
+                select 'ALTER FUNCTION "' || nsp.nspname || '"."' || p.proname || '"(' || pg_get_function_identity_arguments(p.oid) || ') OWNER TO $extra;' as a
+                    from pg_proc p join pg_namespace nsp ON p.pronamespace = nsp.oid where nsp.nspname = 'public'
+                    union all
+                select 'ALTER DATABASE $extra OWNER TO $extra;'
+            LOOP
+                EXECUTE r.a;
+            END LOOP;
+        END \$\$;
+EOSQL
 done
