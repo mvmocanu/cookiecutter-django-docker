d6f5aacdec9f8f3c0e9287aff08ca70854fbc4e2
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-12-19 18:26:51 +0200

    Remove pointless translation.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/admin.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/admin.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib.admin import AdminSite
-from django.utils.translation import gettext_lazy as _
 
 
 class CustomAdminSite(AdminSite):
-    site_header = _('{{ cookiecutter.django_project_name.title() }} v{version}').format(version=settings.PROJECT_VERSION)
+    site_header = f'{{ cookiecutter.django_project_name.title() }} v{settings.PROJECT_VERSION}'
