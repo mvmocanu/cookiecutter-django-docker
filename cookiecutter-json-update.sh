8ae7b3dec2cad07ae277c1dfee3b17c5228f2fc1
Ionel Cristian Mărieș <contact@ionelmc.ro>
2023-03-14 17:41:19 +0200

    Refactor.

--- cookiecutter-json-update.sh
+++ cookiecutter-json-update.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 shopt -s extglob
 
-pip-compile --allow-unsafe cookiecutter-packages.in
+update_field() {
+  field="$1"
+  value="$2"
+  cookiecutter_json=$(cat cookiecutter.json | jq .$field=\"$value\" )
+  echo "$cookiecutter_json" > cookiecutter.json
+}
 
-COOKIECUTTER_JSON=$(cat cookiecutter.json \
- | jq .pip_version=\"$(grep 'pip==' cookiecutter-packages.txt | cut -d= -f3)\" \
- | jq .pip_tools_version=\"$(grep 'pip-tools==' cookiecutter-packages.txt | cut -d= -f3)\" \
- | jq .setuptools_version=\"$(grep 'setuptools==' cookiecutter-packages.txt | cut -d= -f3)\")
-
-echo "$COOKIECUTTER_JSON" > cookiecutter.json
-
+pip-compile --allow-unsafe --quiet cookiecutter-packages.in
+update_field pip_version "$(grep 'pip==' cookiecutter-packages.txt | cut -d= -f3)"
+update_field pip_tools_version "$(grep 'pip-tools==' cookiecutter-packages.txt | cut -d= -f3)"
+update_field setuptools_version "$(grep 'setuptools==' cookiecutter-packages.txt | cut -d= -f3)"
 rm cookiecutter-packages.txt
+
