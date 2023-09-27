1cafd2fdd4d1511ff2ff37897e40cae1bdf0a908
Ionel Cristian Mărieș <contact@ionelmc.ro>
2023-09-27 16:34:42 +0300

    Use strip extras, and pipefail is useless (no pipes).

--- {{cookiecutter.repo_name}}/docker/requirements/entrypoint.sh
+++ {{cookiecutter.repo_name}}/docker/requirements/entrypoint.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-set -xeuo pipefail
+#!/bin/bash -eux
 ls -al requirements
 for req in requirements/*.in; do
-  pip-compile --generate-hashes --allow-unsafe --quiet --upgrade --resolver=backtracking $req
+  pip-compile --generate-hashes --allow-unsafe --quiet --upgrade --strip-extras --resolver=backtracking $req
 done
