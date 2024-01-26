1946ce61d4e20969dd783a0e70c7cfc0a59585fe
Ionel Cristian Mărieș <contact@ionelmc.ro>
2024-01-26 20:02:59 +0200

    Modernize reloader to use docker compose and handle web container better.

--- {{cookiecutter.repo_name}}/docker/reloader/start.sh
+++ {{cookiecutter.repo_name}}/docker/reloader/start.sh
@@ -2,6 +2,9 @@
 #!/bin/bash
 set -euo pipefail
 shopt -s extglob
+declare -A services
+docker --version
+docker compose version
 
 if [ -z "$RELOADER_MONITOR" ]; then
     echo -e "\033[1;33m[$(date -Iseconds)] Reloader is disabled. Exiting ...\033[0m"
@@ -10,7 +13,50 @@ fi
 
 while true; do
     echo -e "\033[1;32m[$(date -Iseconds)] Waiting for changes ...\033[0m"
-    (
+    while read event; do
+        set +x
+        echo -e "\033[1;36m[$(date -Iseconds)] Detected $event changes.\033[0m "
+        if [[ $event =~ .*/static(/|$) ]]; then
+            echo -e "\033[1;34m[$(date -Iseconds)] Running collectstatic ..."
+            (
+                set -x
+                docker compose exec web pysu app django-admin collectstatic --no-input -v0
+            ) || echo -e "\033[1;31m[$(date -Iseconds)] Failed to run collectstatic!"
+        else
+            echo -e "\033[1;34m[$(date -Iseconds)] Attempting restarts ..."
+            for name in $(docker compose ps --services | egrep "$RELOADER_CONTAINERS"); do
+                services[$name]=1
+            done
+            if [[ -z "${!services[*]}" ]]; then
+                echo -e "\033[1;31m[$(date -Iseconds)] No services detected. Perhaps you meant to meant to run more than just the reloader? Try using:"
+                echo -e "\033[1;33m
+      docker compose up reloader web
+
+Or just run everything:
+
+      docker compose up"
+                exit 1
+            fi
+            echo -e "\033[1;34m[$(date -Iseconds)] Restarting ${#services[@]} services: ${!services[@]}"
+            pids=()
+            for name in "${!services[@]}"; do
+                (
+                    if [[ "$name" = web && -p /var/app/run/uwsgi.fifo ]]; then # check if the pipe exists
+                        # and it actually has something connected to it
+                        # (/bin/echo stuff <>path >path would get SIGPIPE if that pipe is dead)
+                        echo '+ echo r > /var/app/run/uwsgi.fifo'
+                        /bin/echo r 1<>/var/app/run/uwsgi.fifo >/var/app/run/uwsgi.fifo || docker compose restart web
+                    else
+                        set -x
+                        docker compose restart "$name"
+                    fi
+                ) &
+                pids+=($!)
+            done
+            wait "${pids[@]}"
+        fi
+        break
+    done < <(
         set -x
         fswatch \
             --recursive \
@@ -24,27 +70,8 @@ while true; do
             --event AttributeModified \
             --event MovedFrom \
             --event MovedTo \
-            --one-per-batch \
             --monitor ${RELOADER_MONITOR:-inotify}_monitor \
             /app/src/
-    ) | while read event; do
-        echo -e "\033[1;36m[$(date -Iseconds)] Detected $event changes.\033[0m "
-        if [[ $event =~ .*/static(/|$) ]]; then
-            echo -e "\033[1;34m[$(date -Iseconds)] Running collectstatic ..."
-            (
-                set -x
-                docker exec ${COMPOSE_PROJECT_NAME}_web_1 pysu app django-admin collectstatic --no-input -v0
-            ) || echo -e "\033[1;31m[$(date -Iseconds)] Failed to run collectstatic!"
-        else
-            echo -e "\033[1;34m[$(date -Iseconds)] Attempting restarts ..."
-            /bin/echo r 1<>/var/app/run/uwsgi.fifo >/var/app/run/uwsgi.fifo || echo -e "\033[31muWSGI is not running!"
-            pids=()
-            for name in $(docker ps --format '{{ .Names }}' | egrep "^$COMPOSE_PROJECT_NAME[-_]($RELOADER_CONTAINERS)"); do
-                (set -x; docker restart $name) &
-                pids+=($!)
-            done
-            wait "${pids[@]}"
-        fi
-    done
+    )
 done
 {%- endraw %}
