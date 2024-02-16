18b1459a1145fabd50ca258f4e15edbe357f649b
Ionel Cristian Mărieș <contact@ionelmc.ro>
2024-02-16 10:53:01 +0200

    Allow using "latest" for guess what.

--- cookiecutter-json-update.sh
+++ cookiecutter-json-update.sh
@@ -5,11 +5,12 @@ shopt -s extglob
 update_field() {
   field="$1"
   value="$2"
-  cookiecutter_json=$(cat cookiecutter.json | jq .$field=\"$value\" )
+  cookiecutter_json=$(cat cookiecutter.json | jq ._$field=\"$value\" | jq ".__prompts__.$field=\"$field (latest='$value')\"" )
   echo "$cookiecutter_json" > cookiecutter.json
 }
 
 pip-compile --allow-unsafe --quiet cookiecutter-packages.in
+update_field django_version "$(grep 'django==' cookiecutter-packages.txt | cut -d= -f3 | cut -d. -f1-2)"
 update_field pip_version "$(grep 'pip==' cookiecutter-packages.txt | cut -d= -f3)"
 update_field pip_tools_version "$(grep 'pip-tools==' cookiecutter-packages.txt | cut -d= -f3)"
 update_field setuptools_version "$(grep 'setuptools==' cookiecutter-packages.txt | cut -d= -f3)"
