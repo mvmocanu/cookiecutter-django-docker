2cc8e42bae91fdb2b33eb522d0fe7620eba135d6
Ionel Cristian Mărieș <contact@ionelmc.ro>
2024-02-24 04:00:44 +0200

    Add support for using uv instead of pip-tools.

--- cookiecutter-json-update.sh
+++ cookiecutter-json-update.sh
@@ -5,11 +5,12 @@ shopt -s extglob
 update_field() {
   field="$1"
   value="$2"
-  cookiecutter_json=$(cat cookiecutter.json | jq ._$field=\"$value\" | jq ".__prompts__.$field=\"$field (latest='$value')\"" )
+  cookiecutter_json=$(cat cookiecutter.json | jq ._$field=\"$value\" | jq ".__prompts__.$field=\"$field (latest=$value)\"" )
   echo "$cookiecutter_json" > cookiecutter.json
 }
 
-pip-compile --allow-unsafe --quiet cookiecutter-packages.in
+uv pip compile --allow-unsafe --quiet cookiecutter-packages.in --output-file=cookiecutter-packages.txt
+update_field uv_version "$(grep 'uv==' cookiecutter-packages.txt | cut -d= -f3)"
 update_field django_version "$(grep 'django==' cookiecutter-packages.txt | cut -d= -f3 | cut -d. -f1-2)"
 update_field pip_version "$(grep 'pip==' cookiecutter-packages.txt | cut -d= -f3)"
 update_field pip_tools_version "$(grep 'pip-tools==' cookiecutter-packages.txt | cut -d= -f3)"
--- {{cookiecutter.repo_name}}/docker/requirements/entrypoint.sh
+++ {{cookiecutter.repo_name}}/docker/requirements/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eux
 ls -al requirements
 for req in requirements/*.in; do
+{% if cookicutter.uv_enabled %}
+  uv pip compile --generate-hashes --allow-unsafe --quiet --upgrade --strip-extras --resolver=backtracking $req --output-file=${req%.*}.txt
+{% else %}
   pip-compile --generate-hashes --allow-unsafe --quiet --upgrade --strip-extras --resolver=backtracking $req
+{% endif %}
 done
