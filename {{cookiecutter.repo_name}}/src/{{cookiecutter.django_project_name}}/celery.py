4c0a61f0d926beec4fd3c87b33e2e2bb19e9d2dc
Ionel Cristian Mărieș <contact@ionelmc.ro>
2024-05-09 14:46:18 +0300

    Remove this default hardcoding.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/celery.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/celery.py
@@ -2,8 +2,7 @@ import os
 
 from celery import Celery
 
-# set the default Django settings module for the "celery" program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.django_project_name }}.settings")
+assert "DJANGO_SETTINGS_MODULE" in os.environ
 
 app = Celery("{{ cookiecutter.django_project_name }}")
 app.config_from_object("django.conf:settings", namespace="CELERY")
