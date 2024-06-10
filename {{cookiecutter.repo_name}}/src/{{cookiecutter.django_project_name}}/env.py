daebad044b4be071fc6e5f1db99bab0003d6edc0
Ionel Cristian Mărieș <contact@ionelmc.ro>
2024-06-10 18:26:58 +0300

    Add some annotations, and a path function.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/env.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/env.py
@@ -1,12 +1,13 @@
 import builtins
 import os
+import pathlib
 
 
-def get(key):
+def get(key) -> builtins.str | None:
     return os.environ.get(key)
 
 
-def str(key, default=Ellipsis):
+def str(key, default=Ellipsis) -> builtins.str:
     if default is Ellipsis:
         if bool("__strict_env__", True):
             return os.environ[key]
@@ -16,7 +17,7 @@ def str(key, default=Ellipsis):
         return os.environ.get(key, default)
 
 
-def list(key, default=None, separator=","):
+def list(key, default=None, separator=",") -> builtins.list:
     value = str(key, default)
     if value is None:
         return []
@@ -24,12 +25,19 @@ def list(key, default=None, separator=","):
         return value.split(separator)
 
 
-def int(key, default):
+def int(key, default) -> builtins.int:
     return builtins.int(str(key, default))
 
 
-def bool(key, default):
+def bool(key, default) -> builtins.bool:
     if key in os.environ:
         return os.environ.get(key).lower() in ("yes", "true", "y", "1")
     else:
         return default
+
+
+def path(key, default=Ellipsis) -> pathlib.Path:
+    value = str(key, default)
+    value = pathlib.Path(value)
+    assert value.exists(), f"{value!r} does not exist"
+    return value
