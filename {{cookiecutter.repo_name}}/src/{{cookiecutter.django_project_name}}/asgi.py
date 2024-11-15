cfe204af49d70d86d9fa8c454ac5acb41105b834
Ionel Cristian Mărieș <contact@ionelmc.ro>
2024-11-15 13:44:13 +0200

    Fix links, update nginx config, add network disable default in pytest config.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/asgi.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/asgi.py
@@ -4,7 +4,7 @@ ASGI config for {{ cookiecutter.django_project_name }} project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/stable/howto/deployment/asgi/
 """
 
 from django.core.asgi import get_asgi_application
--- {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/wsgi.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.django_project_name}}/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for {{ cookiecutter.django_project_name }} project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 """
 
 from django.core.wsgi import get_wsgi_application
